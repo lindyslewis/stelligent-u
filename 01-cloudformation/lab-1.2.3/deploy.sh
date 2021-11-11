f3cf9125473e9cd3bd9f86420263b31d35262675
Lindy Lewis <lindyslewis@gmail.com>
2021-11-11 11:16:35 -0600

    lab-1.2.3

---
+++ 01-cloudformation/lab-1.2.3/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Use this script as a quick execute shim for testing cfn stacks
+STACKNAME="lindy-lewis-test-stack-su-1-2-3-NEW"
+TESTFILE="file://iam_user.yaml"
+PARAMFILE="file://parameters.json"
+
+# Create Stack
+aws cloudformation create-stack --stack-name $STACKNAME --capabilities CAPABILITY_NAMED_IAM --template-body $TESTFILE --parameters $PARAMFILE
+
+# Delete Stack
+# aws cloudformation delete-stack --stack-name $STACKNAME --capabilities CAPABILITY_NAMED_IAM
+
+# Check if a Stack Exists
+# aws cloudformation describe-stacks --stack-name $STACKNAME
+
+# Enable Termination Protection
+# aws cloudformation update-termination-protection --enable-termination-protection --stack-name $STACKNAME
+
+# Disable Termination Protection
+# aws cloudformation update-termination-protection --no-enable-termination-protection --stack-name $STACKNAME
+
+# List Exports
+# aws cloudformation list-exports
---
+++ 01-cloudformation/lab-1.2.3/guide.md
@@ -0,0 +1,11 @@
+# Lab 1.2.3 Guide
+
+## Lab 1.2.3: Importing another Stack's Exports
+
+Create a *new* CFN template that describes an IAM User and applies to it
+the Managed Policy ARN created by and exported from the previous Stack.
+
+- Create this new Stack.
+
+- [List all the Stack Imports](https://docs.aws.amazon.com/cli/latest/reference/cloudformation/list-imports.html)
+  in that stack's region.
