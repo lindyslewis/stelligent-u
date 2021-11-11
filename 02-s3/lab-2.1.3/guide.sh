6d106bdf6e4417c96d545f2e35a686ab2930e633
Lindy Lewis <lindyslewis@gmail.com>
2021-11-11 12:18:52 -0600

    lab-2.1.3

--- 02-s3/lab-2.1.3/guide.sh
+++ 02-s3/lab-2.1.3/guide.sh
@@ -5,13 +5,13 @@
 Add a private file to your data directory. Then, upload the directory to your
 bucket again **without including the private file**.
 
-`touch data/private`
-`aws s3 sync ./data s3://stelligent-u-lindylewis --exclude 'private'`
-`aws s3 cp ./data s3://stelligent-u-lindylewis --exclude 'private' --recursive`
+> `touch data/private`
+> `aws s3 sync ./data s3://stelligent-u-lindylewis --exclude 'private'`
+> `aws s3 cp ./data s3://stelligent-u-lindylewis --exclude 'private' --recursive`
 
 - Verify after uploading that the file does not exist in the bucket.
 
-`aws s3 ls s3://stelligent-u-lindylewis`
+> `aws s3 ls s3://stelligent-u-lindylewis`
 
 - Did you find two different ways to accomplish this task? If not, make sure to
   read the [documentation on sync flags](https://docs.aws.amazon.com/cli/latest/reference/s3/sync.html).
